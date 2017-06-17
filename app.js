a5d6313a9af42de7be02796453675e905f198d65
Maxim Kukhtenkov <maxim.kht@gmail.com>
2017-06-17 17:36:52 -0400

    Add data fetched from app server

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const axios = require('axios');
+const exec = require('child_process').exec;
+
+/* ----
+ * Conf
+ * ---- */
+
+const APP_SERVER_ENDPOINT = 'http://172.31.33.151/api/';
+
+/* ----
+ * App
+ * ---- */
 
 const app = express();
 
 app.set('view engine', 'nunjucks');
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app
 });
 
-app.get('/', function (req, res) {
-  res.render('index.html');
+/* ------
+ * Routes
+ * ------ */
+
+app.get('/', (req, res) => {
+  const instance = axios.create({ baseURL: APP_SERVER_ENDPOINT });
+
+  axios
+    .all([
+      instance.get('messages'),
+      instance.get('gallery'),
+      instance.get('json-placeholder')
+    ])
+    .then(axios.spread((messages, gallery, jsonPlaceholder) => {
+      res.render('index.html', {
+        messages: messages.data,
+        gallery: gallery.data,
+        jsonPlaceholder: jsonPlaceholder.data
+      });
+    }));
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
